88410251b9f4d872f4558d7b4fc4c73bedbe0baa
liu <lzjun567@gmail.com>
2014-02-28 10:58:32 +0800

    update fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,25 @@
-from fabric.api import local
+from fabric.api import local, settings, abort, cd, env, run
+
+env.hosts = ['root@foofish.net']
 
 def prepare_deploy():
-    local("python ./manage.py test apps.blog")
+    test() 
     local('git add *.py *.html && git commit')
     local('git push origin master')
+
+def test():
+    with settings(warn_only=True):
+        result = local("python ./manage.py test apps.blog", capture=True)
+        if result.failed and not confirm("Test failed, Continue anyway"):
+            abort("aborting at user request")
+
+def deploy():
+    project_dir = '/home/django_blog'
+    with cd(project_dir):
+        run("git pull origin master")
+        run("supervistorctl restart foofish")
+
+def go():
+    prepare_deploy()
+    deploy()
+
