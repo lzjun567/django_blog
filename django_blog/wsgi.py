3b99b38e76f0fdc5950b7e2d239cbdee6104064d
liuzhijun <lzjun567@gmail.com>
2014-03-22 02:58:58 +0800

    update

--- django_blog/wsgi.py
+++ django_blog/wsgi.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings.prod")
 
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
