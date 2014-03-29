90f632602387a499f72b5e36da9fc0a1c49f0046
liuzhijun <lzjun567@gmail.com>
2014-03-29 23:06:51 +0800

    remove markdown2

--- apps/blog/templatetags/myapp_markup.py
+++ apps/blog/templatetags/myapp_markup.py
@@ -1,5 +1,5 @@
 #! coding=utf-8
-import markdown2
+#import markdown2
 import markdown
 
 from django import template
@@ -9,17 +9,17 @@ from django. utils.safestring import mark_safe
 
 register = template.Library()
 
-@register.filter(is_safe=True)
-@stringfilter
-def md2(value):
-    '''
-        目前markdown2 无法处理井号（####）标题
-    '''
-    return mark_safe(markdown2.markdown(
-                force_unicode(value),
-                safe_mode=True)
-            )
-    return mark_safe(markdown2.markdown(value))
+#@register.filter(is_safe=True)
+#@stringfilter
+#def md2(value):
+#    '''
+#        目前markdown2 无法处理井号（####）标题
+#    '''
+#    return mark_safe(markdown2.markdown(
+#                force_unicode(value),
+#                safe_mode=True)
+#            )
+#    return mark_safe(markdown2.markdown(value))
 
 @register.filter(is_safe=True)
 @stringfilter
