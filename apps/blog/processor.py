7827056a43a1fa963db3a5d1d3ed13da3a8ad2ea
liuzhijun <lzjun567@gmail.com>
2014-04-12 10:07:28 +0800

    add tag cloud

--- apps/blog/processor.py
+++ apps/blog/processor.py
@@ -1,8 +1,14 @@
-from .models import Tag
+#! encoding=utf-8
+from django.db import connection
 def tag_list(request):
-    tags = Tag.objects.all()
-    ctx = {'tag1s': tags[0:len(tags)/2],
-           'tag2s': tags[len(tags)/2:]
-    }
+    '''获取每个标签的文章数量'''
+    cursor = connection.cursor()
+    sql = "SELECT title, c FROM blog_tag RIGHT JOIN \
+                (SELECT tag_id AS tid, COUNT(*) AS c \
+                    FROM blog_blog_tags GROUP BY tag_id) AS t2\
+            ON blog_tag.id=t2.tid";
+    cursor.execute(sql) 
+    tags = cursor.fetchall()
+    ctx = {'tags':tags}
     return ctx 
 
