ae0024ea89de07e023e903ea8391d58813a4532d
liuzhijun <lzjun567@qq.com>
2015-12-07 00:19:35 +0800

    Update URL

--- urls.py
+++ urls.py
@@ -13,7 +13,8 @@ sitemaps = {
 
 urlpatterns = patterns('',
                        url(r'^about$', 'apps.blog.views.about', name='about'),
-                       url(r'', include('apps.blog.urls', namespace='blog')),
+                       url(r'^blog/', include('apps.blog.urls', namespace='blog')),
+                       url(r'^$', include('apps.blog.urls', namespace='blog')),
                        url(r'^wedding/', include('apps.wedding.urls', namespace='wedding')),
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'404', 'apps.blog.views.not_found'),
