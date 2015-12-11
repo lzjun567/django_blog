4fc5e06dbba0dcc42e57ab8821afecaf03c3b80d
liuzhijun <lzjun567@qq.com>
2015-12-11 14:05:56 +0800

    Update filter

--- django_blog/sitemaps.py
+++ django_blog/sitemaps.py
@@ -1,6 +1,5 @@
 #!encoding=utf-8
 from django.contrib.sitemaps import Sitemap
-from django.core.urlresolvers import reverse
 from apps.blog.models import Blog
 
 
@@ -9,11 +8,10 @@ class BlogSitemap(Sitemap):
     priority = 0.5
 
     def items(self):
-        return Blog.objects.all()
+        return Blog.objects.filter(is_public=True).filter(status='p')
 
     def lastmod(self, item):
-        return item.update_time
+        return item.add_time
 
     def location(self, item):
         return r'/blog/%d/%s' % (item.id, item.link)
-
