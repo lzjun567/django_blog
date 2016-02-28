689a8362c4802828cbf2a817ff7bf4197fd6db4b
liuzhijun <lzjun567@qq.com>
2016-02-28 23:23:33 +0800

    Add tag cloud

--- apps/blog/urls.py
+++ apps/blog/urls.py
@@ -4,9 +4,11 @@
 from django.conf.urls import patterns, url
 from .views import BlogDetailView
 from .views import BlogListView
+from .views import TagListView
 
 urlpatterns = patterns('apps.blog.views',
                        url(r'^tag/(?P<tag_name>\w+)$', BlogListView.as_view(), name='tag'),
+                       url(r'^tags$', TagListView.as_view(), name='tag_list'),
                        url(r'^$', BlogListView.as_view(), name="home"),
                        url(r'^(?P<pk>\d+)/(?P<blog_link>[\w,-]*)$', BlogDetailView.as_view(), name='blog_detail'),
 
--- apps/blog/views.py
+++ apps/blog/views.py
@@ -8,6 +8,43 @@ from .models import Blog, Tag
 from django.core.exceptions import PermissionDenied
 
 
+class TagListView(ListView):
+    template_name = 'tag_list.html'
+    context_object_name = 'tag_list'
+    model = Tag
+
+    colors = ['#ccc', "#adadad", '#8e8e8e', '#6f6f6f', '4f4f4f', '#303030', '#111', '#000']
+    max_font_size, min_font_size = 33, 12
+    font_sizes = [min_font_size, 15, 18, 21, 24, 27, 30, max_font_size]
+
+    def get_context_data(self, **kwargs):
+        context = super(TagListView, self).get_context_data(**kwargs)
+        tag_list = context.get("tag_list")
+        # 有博文的tag
+        tag_list_have_blog = []
+        for tag in tag_list:
+            blog_count = Blog.objects.filter(tags__pk=tag.id).count()
+            if blog_count > 0:
+                tag.blog_count = blog_count
+                tag_list_have_blog.append(tag)
+
+        max_count = max(tag_list_have_blog, key=lambda tag: tag.blog_count).blog_count
+        min_count = min(tag_list_have_blog, key=lambda tag: tag.blog_count).blog_count
+        step = (self.max_font_size - self.min_font_size) / (max_count - min_count)
+        print("step:", step)
+        for tag in tag_list_have_blog:
+            size = round(self.min_font_size + (tag.blog_count - min_count) * step)
+            tag_font_size = min(self.font_sizes, key=lambda x: abs(x-size))
+
+            color = self.colors[self.font_sizes.index(tag_font_size)]
+
+            tag.color = color
+            tag.font_size = tag_font_size
+
+        context['tag_list'] = tag_list_have_blog
+        return context
+
+
 class BlogListView(ListView):
     template_name = 'post_list.html'
     paginate_by = settings.PAGE_SIZE
@@ -28,7 +65,6 @@ class BlogListView(ListView):
         tag_name = self.kwargs.get('tag_name')
         if tag_name:
             context['tag_title'] = tag_name
-            # tag = Tag.objects.get(title=tag_name)
             context['tag_description'] = ''
         return context
 
