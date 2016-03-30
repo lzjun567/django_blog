b7f0855c7d64d9fee753c2255134cffac56774f3
liuzhijun <lzjun567@qq.com>
2016-03-30 22:52:14 +0800

    Fix publish

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -17,12 +17,33 @@ def submit_row(context):
     移除"保存继续编辑"按钮
     添加"保存为草稿"按钮
     """
-    ctx = original_submit_row(context)
+    # ctx = original_submit_row(context)
+    change = context['change']
+    is_popup = context['is_popup']
+    save_as = context['save_as']
+    show_save = context.get('show_save', True)
+    show_save_and_continue = context.get('show_save_and_continue', True)
+    from django.template.context import Context
+
+    ctx = Context(context)
     ctx.update({
-        'show_save_and_add_another': False,
+        'show_delete_link': True,
+
+        'show_save_as_new': not is_popup and change and save_as,
+        'show_save_and_add_another': (
+            context['has_add_permission'] and not is_popup and
+            (not save_as or context['add'])
+        ),
+        'show_save_and_continue': not is_popup and context['has_change_permission'] and show_save_and_continue,
+        'show_save': False,
         'show_save_as_draft': True,
-        'show_save_and_continue': False,
     })
+    # ctx.update({
+    #     'show_save_and_add_another': False,
+    #     'show_save_as_draft': True,
+    #     'show_save_and_continue': False,
+    # })
+    print(ctx)
     return ctx
 
 
@@ -62,12 +83,11 @@ class BlogAdmin(admin.ModelAdmin):
     def make_published(self, request, queryset):
         rows_updated = 0
         for entry in queryset:
-            if entry.status != 'p':
-                entry.status = 'p'
-                rows_updated += 1
-                if entry.publish_time is None:
-                    entry.publish_time = datetime.datetime.now()
-                entry.save()
+            entry.status = 'p'
+            rows_updated += 1
+            if entry.publish_time is None:
+                entry.publish_time = datetime.datetime.now()
+            entry.save()
         message_bit = "%s 篇博客 " % rows_updated
         self.message_user(request, "%s 成功发布" % message_bit)
 
@@ -76,19 +96,21 @@ class BlogAdmin(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
         obj.author = request.user
         if '_save' in request.POST.keys():
+            obj.publish_time = datetime.datetime.now()
             obj.status = Blog.STATUS_CHOICES[1][0]
         elif '_save_as_draft' in request.POST.keys():
             obj.status = Blog.STATUS_CHOICES[0][0]
         super(BlogAdmin, self).save_model(request, obj, form, change)
         # obj.save()
 
-    def response_action(self, request, queryset):
-        print(request)
-        return super(BlogAdmin, self).response_action(request, queryset)
+    def change_view(self, request, object_id, form_url='', extra_context=None):
+        more_context = {}
+        more_context.update(extra_context or {})
+        return super().change_view(request, object_id, form_url, more_context)
 
-    def response_change(self, request, obj):
-        print(obj)
-        return super(BlogAdmin, self).response_change(request, obj)
+
+
+
 
 
 class TagAdmin(admin.ModelAdmin):
