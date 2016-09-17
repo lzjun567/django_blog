01eab1714e0a9cb42b4665d0049d1d40f033463b
liuzhijun <lzjun567@qq.com>
2016-09-17 17:04:51 +0800

    remove theme

--- apps/blog/context_processors.py
+++ apps/blog/context_processors.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
+
+from django.conf import settings
+
 """
 上下文处理器，函数返回的字典对象会自动加入到Context对象中
 Context对象中的值可以使用在所有模版中
 """
-from django.db import connection
 
 
 def theme(request):
     """
     加载博客主题模版配置
-    :param request:
-    :return:
     """
-    from django.conf import settings
-
     theme_data = dict()
     theme_data['site_title'] = getattr(settings, "SITE_TITLE")
     theme_data['site_subtitle'] = getattr(settings, "SITE_SUBTITLE")
+    theme_data['keywords'] = getattr(settings, "KEYWORDS", '')
+
     theme_data['scheme'] = getattr(settings, "SCHEME", "")
     theme_data['use_motion'] = "use-motion" if getattr(settings, "USE_MOTION") else ""
     theme_data['google_site_verification'] = getattr(settings, "GOOGLE_SITE_VERIFICATION", "")
@@ -24,7 +24,6 @@ def theme(request):
     theme_data['qihu_site_verification'] = getattr(settings, "QIHU_SITE_VERIFICATION", "")
     theme_data['fancybox'] = getattr(settings, "FANCYBOX", True)
     theme_data['version'] = getattr(settings, "VERSION", '')
-    theme_data['keywords'] = getattr(settings, "KEYWORDS", '')
     theme_data['rss'] = getattr(settings, "RSS", '')
     theme_data['favicon'] = getattr(settings, "FAVICON", '')
     theme_data['sidebar'] = getattr(settings, "SIDEBAR", {})
@@ -37,4 +36,3 @@ def theme(request):
     theme_data['google_analytics'] = getattr(settings, "GOOGLE_ANALYTICS", None)
 
     return {"theme": theme_data}
-
